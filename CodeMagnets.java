34d24c2835288f8a73e01f16c1b260aaeb876d63
Emmanuel Acheampong <acheamponge@berea.edu>
2019-09-07 10:07:52 -0400

    Add files via upload

---
+++ CodeMagnets.java
@@ -0,0 +1,27 @@
+public class CodeMagnets {
+    public static void main(String[] args)
+    {
+        int x =3;
+
+        while (x > 0)
+        {
+            if(x>2)
+            {
+                System.out.print("a");
+            }
+            if(x == 2)
+            {
+                System.out.print("b c");
+            }
+            x--;
+            System.out.print("~");
+            
+            if (x==1)
+            {
+                System.out.print("d");
+            }
+
+
+        }
+    }
+}
