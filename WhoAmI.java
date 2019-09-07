bc811c08bf9ef252f29d12bbd0e91668ca7be449
Emmanuel Acheampong <acheamponge@berea.edu>
2019-09-07 18:35:15 -0400

    Add files via upload

---
+++ WhoAmI.java
@@ -0,0 +1,19 @@
+public class WhoAmI {
+    public static void main(String[] args)
+    {
+        System.out.println("WHO AM I?");
+        System.out.println("I am compiled from a .java file. --- CLASS");
+        System.out.println("My instance variable values can be different from my buddy's values --- OBJECT");
+        System.out.println("I behave like a template --- CLASS");
+        System.out.println("I like to do stuff --- OBJECT, METHOD");
+        System.out.println("I can have many methods --- OBJECT, CLASS");
+        System.out.println("I represent 'state' --- INSTANCE VARIABLE");
+        System.out.println("I have behaviors --- OBJECT, CLASS");
+        System.out.println("I am located in objects --- METHODS, INSTANCE VARIABLES");
+        System.out.println("I live on the heap --- OBJECT");
+        System.out.println("I am used to create object instances --- CLASS");
+        System.out.println("My state can change --- OBJECT, INSTANCE VARIABLE");
+        System.out.println("I declare methods --- CLASS");
+        System.out.println("I can change at runtime --- OBJECT, INSTANCE VARIABLE");
+    }
+}
