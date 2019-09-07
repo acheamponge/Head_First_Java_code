6410b23c6421a5d3daf1cfd35ecda9046ec19181
Emmanuel Acheampong <acheamponge@berea.edu>
2019-09-07 11:02:39 -0400

    Add files via upload

---
+++ JavaCross7.java
@@ -0,0 +1,34 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
+public class JavaCross7 {
+    public static void main(String[] args)
+    {
+        System.out.println("ACROSS");
+        System.out.println("4. Command-line invoker: JAVA");
+        System.out.println("6. Back again: LOOP");
+        System.out.println("8. Can't go both ways: BRANCH");
+        System.out.println("9. Acronym for your laptop's power: DC");
+        System.out.println("12. Number variable type: INT");
+        System.out.println("13. Acronym for a chip: IC");
+        System.out.println("14. Say Something: SYSTEMOUTPRINT");
+        System.out.println("18. Quite a crew of characters: STRING");
+        System.out.println("19. Announce a new class or method: DECLARE");
+        System.out.println("21. What's a prompt good for?: COMMAND ");
+
+
+        System.out.println("\n" +"\n");
+        System.out.println("DOWN");
+        System.out.println("1. Not an integer(or ___ your boat): FLOAT");
+        System.out.println("2. Come back empty-handed: VOID");
+        System.out.println("3. Open house: PUBLIC");
+        System.out.println("5. Things' holders: ARRAYS");
+        System.out.println("7. Until attitudes improve: WHILE");
+        System.out.println("10. Source code consumer: COMPILER");
+        System.out.println("11. Can't pin it down: VARIABLE");
+        System.out.println("13. Dept. of LAN jockeys: IT");
+        System.out.println("15. Shocking modifier: STATIC");
+        System.out.println("16. Just gotta have one: MAIN");
+        System.out.println("17. How to get things done: METHOD");
+        System.out.println("20. Bytecode consumer: JVM");
+    }
+}
