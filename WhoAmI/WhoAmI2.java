0a969d6b6e3c6e8bc1407f6830a802c7b18679f5
Emmanuel Acheampong <acheamponge@berea.edu>
2019-09-08 13:17:22 -0400

    Add files via upload

---
+++ WhoAmI/WhoAmI2.java
@@ -0,0 +1,17 @@
+public class WhoAmI2 {
+    public static void main(String[] args) {
+        System.out.println("A class can have any number of these --- INSTANCE VARIABLES, GETTER, SETTER METHODS.");
+        System.out.println("A method can have only one of these --- RETURN");
+        System.out.println("This can be implicitly promoted --- RETURN, ARGUMENT");
+        System.out.println("I prefer my instance variables private --- ENCAPSULATION");
+        System.out.println("It really means 'make a copy' --- PASS BY VALUE");
+        System.out.println("Only setters should update these --- INSTANCE VARIABLES");
+        System.out.println("A method can have many of these --- ARGUMENTS");
+        System.out.println("I return something by definition --- GETTER");
+        System.out.println("I shouldn't be used with instance variables --- PUBLIC");
+        System.out.println("I can have many arguments --- METHODS");
+        System.out.println("By definition, I can take one argument --- SETTER");
+        System.out.println("These help create encapsulation --- GETTER, SETTER, PUBLIC, PRIVATE");
+        System.out.println("I always fly solo --- RETURN");
+    }
+}
